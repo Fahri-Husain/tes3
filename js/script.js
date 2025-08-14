c169a87db85906dacd02c48f8d4d83a22dfd24dc
FahriHusain <fahrihusain243@gmail.com>
2025-08-15 00:35:29 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,5 +3,6 @@ setTimeout(() => {
   // Tandai animasi sudah selesai
   sessionStorage.setItem("netflixDone", "true");
   // Redirect ke Index.html
-  window.location.href = "Halaman2.html";
+  window.location.href = "halaman2.html";
 }, 5500); // durasi animasi
+
